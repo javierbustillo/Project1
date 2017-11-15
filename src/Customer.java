89b943cc8d1a84e5543dcaeccbdbaafd92b0ba4c
javierbustillo <javier.bustillo@upr.edu>
2017-11-15 17:54:14 -0400

    Finished customer class and created Main

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,20 @@
 
 public class Customer {
+	// instance variables
+	private int arrival;
+	private String id;
+	private int timeToPrepare;
+	private int costOfOrder;
+	private int patience;
+	
+	public Customer(int arrival, String id, int timeToPrepare, int costOfOrder, int patience) {
+		this.arrival = arrival;
+		this.id = id;
+		this.timeToPrepare = timeToPrepare;
+		this.costOfOrder = costOfOrder;
+		this.patience = patience;
+	}
+	
+	
 
 }
