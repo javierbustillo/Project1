0dd83eff71db1201897d6f72ba1def2d3388a0ee
javierbustillo <javier.bustillo@upr.edu>
2017-11-25 10:44:04 -0400

    Finished restaurant ADT

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -4,13 +4,48 @@ public class Restaurant {
 	private Customer[] customers;
 	private int turns;
 	private float profit;
-	private int customersSatisfied;
+	private int unsatisfiedCustomers;
+	private Boolean isOpen;
 	
 	public Restaurant(Customer[] customers) {
 		this.customers = customers;
 		turns = 0;
 		profit = 0;
-		customersSatisfied = 0;
+		unsatisfiedCustomers = 0;
+		isOpen = true;
 	}
+	
+	public void closeRestaurant() {
+		isOpen = false;
+	}
+	
+	public void takeNewOrder() {
+		
+	}
+	
+	public void updateCustomerStatus() {
+		
+	}
+	
+	public void simulate() {
+		
+	}
+	
+	public Boolean isOpen() {
+		return isOpen;
+	}
+	
+	public int getTurns() {
+		return turns;
+	}
+	
+	public float getProfit() {
+		return profit;
+	}
+	
+	public int getUnsatisfiedCustomers() {
+		return unsatisfiedCustomers;
+	}
+	
 
 }
