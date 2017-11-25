6306e42610d3a5704ac4236a180efc21c139128a
javierbustillo <javier.bustillo@upr.edu>
2017-11-25 10:35:10 -0400

    Added constructor for restaurant

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,13 +1,16 @@
 
 public class Restaurant {
-	
+	//instance variables
 	private Customer[] customers;
 	private int turns;
 	private float profit;
 	private int customersSatisfied;
 	
 	public Restaurant(Customer[] customers) {
-		
+		this.customers = customers;
+		turns = 0;
+		profit = 0;
+		customersSatisfied = 0;
 	}
 
 }
