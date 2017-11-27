0b0a87292e39e0b6992ccc398a43a3cb34ca2f39
rotcehxp <hector.diaz6@upr.edu>
2017-11-26 22:26:47 -0400

    Minor changes in Pac's Restaurant.

---
+++ src/PacRestaurant.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class PacRestaurant extends Restaurant {
+	
+	ArrayList<Customer> customersInLine;
+
+	public PacRestaurant(Customer[] customers) {
+		super(customers);
+		customersInLine = new ArrayList<Customer>();
+	}
+	
+
+	@Override
+	public void takeNewOrder() {
+		// TODO Auto-generated method stub
+		if(customersInLine.size()>0&&wait==0){
+			int customerSelectedIndex = 0;
+			for(int i=0; i<customersInLine.size(); i++){
+				if(customersInLine.get(i).getTimeToPrepare()<customersInLine.get(customerSelectedIndex).getTimeToPrepare()){
+					customerSelectedIndex = i;
+				}
+			}
+			setWait(customersInLine.get(customerSelectedIndex).getTimeToPrepare());
+			addToProfit(customersInLine.get(customerSelectedIndex).getCostOfOrder());
+			customersInLine.remove(customerSelectedIndex);
+			customersInRestaurant--;
+		}
+	}
+	
+	
+	@Override
+	public void updateCustomerStatus() {
+		// TODO Auto-generated method stub
+		while(!customersInLine.isEmpty()&&customersInLine.get(0).getArrival()+customersInLine.get(0).getPatience()<currentTurn){
+			customersInLine.remove(0);
+			unsatisfiedCustomers++;
+			customersInRestaurant--;
+		}
+	}
+
+	@Override
+	public void receiveCustomers() {
+		// TODO Auto-generated method stub
+		for(int i=interactionCounter; i<customers.length&&customers[i].getArrival()<=currentTurn; i++){
+			customersInLine.add(customers[i]);
+			customersInRestaurant++;
+			interactionCounter++;	
+		}	
+	}
+
+}
